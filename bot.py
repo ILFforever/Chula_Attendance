4a911ed2d435e45530f065c5035cb056ec374bbf
ILFforever <hammymukura@gmail.com>
2026-02-23 18:11:23 +0700

    updated bot test fly workflow

--- bot.py
+++ bot.py
@@ -156,7 +156,7 @@ class AttendanceLogger:
             return
 
         chrome_options = Options()
-        # chrome_options.add_argument("--headless=new")  # DEBUG: visible GUI
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
